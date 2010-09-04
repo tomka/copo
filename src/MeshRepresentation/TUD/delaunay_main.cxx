34838affdf865a983ef54a278a0a083215df91fb
Tom Kazimiers <tom@voodoo-arts.net>
2010-09-04 15:41:24 +0200

    Remove conflicting Voronoi implementations

--- src/MeshRepresentation/TUD/delaunay_main.cxx
+++
@@ -1,7 +0,0 @@
-#include "gl_delaunay_mesh_draw.h"
-#include <cgv/gui/fltk/register.h>
-
-//cgv::gui::fltk::factory_registration<gl_delaunay_mesh_draw>
-//	gl_point_cloud_drawable_registration("delaunay", 'y');
-
-
